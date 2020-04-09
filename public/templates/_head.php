ae0460a15da5f874489d7903dced1cfc1b675198
Gunharth Randolf <hello@gunharth.io>
2020-04-09 08:46:34 +0200

    new static folder

--- public/templates/_head.php
+++ public/templates/_head.php
@@ -11,8 +11,8 @@ $basepath = '/';
     <title><?php echo $title; ?></title>
     <meta name="description" content="">
     <meta name="theme-color" content="00908F">
-    <link rel="stylesheet" href="https://use.typekit.net/nvh1bxa.css">
-    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="//use.typekit.net/nvh1bxa.css">
+    <link href="//fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700&display=swap" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
     <link type="text/css" rel="stylesheet" href="//cdn.jsdelivr.net/jquery.jssocials/1.4.0/jssocials.css" />
     <link type="text/css" rel="stylesheet" href="//cdn.jsdelivr.net/jquery.jssocials/1.4.0/jssocials-theme-plain.css" />
