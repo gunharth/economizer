3cd276c36da2c6f71ddc29614cfd6cc9c0f28083
Gunharth Randolf <hello@gunharth.io>
2021-02-23 17:41:18 +0100

    seperate smtp credetials

--- public/mailhandler/brochures.php
+++ public/mailhandler/brochures.php
@@ -22,12 +22,9 @@ try {
     //Server settings
     //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'mail.gandi.net';                    // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 's.sporn@bit-pool.com';                     // SMTP username
-    $mail->Password   = 'ruti$4WB96!';                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    include_once('smtp_secrets.php');
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption
+    $mail->Port = 587; // TCP port to connect to
 
     //Recipients
     $mail->setFrom('s.sporn@bit-pool.com', 'Mailer');
